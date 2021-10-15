73d7f301788e092a55eaf252ec1d2ccf5ea08ddc
Edson Macamo <edsonmacamo85@gmail.com>
2021-10-15 12:02:02 +0200

    Adicionando Hoks

--- src/app/main-lifecycle/lifecycle-child/lifecycle-child.component.ts
+++ src/app/main-lifecycle/lifecycle-child/lifecycle-child.component.ts
@@ -1,18 +1,32 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-lifecycle-child',
   templateUrl: './lifecycle-child.component.html',
   styleUrls: ['./lifecycle-child.component.css']
 })
-export class LifecycleChildComponent implements OnInit {
+export class LifecycleChildComponent implements OnInit, OnDestroy, OnChanges {
   
   @Input() name:string;
   @Input() age:number;
   @Input() food:string;
-  constructor() { }
+  constructor() {
+    console.log(this.name + " - constructor")
+   }
 
-  ngOnInit(): void {
+  ngOnInit(){
+    console.log(this.name + " - OnInit")
   }
-
+   ngOnDestroy(){
+    console.log(this.name + " - OnDestroy")
+   }
+   ngAfterContentInit(){
+    console.log(this.name + " - ngAfterContentInit")
+   }
+   ngAfterViewInit(){
+    console.log(this.name + " - ngAfterViewInit")
+   }
+   ngOnChanges(changes:SimpleChanges){
+    console.log(this.name + " - OnChanges")
+   }
 }

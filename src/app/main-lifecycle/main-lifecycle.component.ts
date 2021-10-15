94eaa172ea03d57cfe01830f95bc47b5546a4c24
Edson Macamo <edsonmacamo85@gmail.com>
2021-10-15 13:28:07 +0200

    crud

--- src/app/main-lifecycle/main-lifecycle.component.ts
+++ src/app/main-lifecycle/main-lifecycle.component.ts
@@ -13,23 +13,38 @@ export class MainLifecycleComponent implements OnInit {
   public name:string;
   public age:number;
   public food:string;
-  public editClient:Client = null as any;
+  public editClient:number = -1;
   constructor() { }
 
   ngOnInit(): void {
   }
 
   save(){
-    if(this.editClient ==null){
+    if(this.editClient == -1){
     this.clients.push({
       name:this.name,
       age:this.age,
       food:this.food
     });
+  }else{
+    this.clients[this.editClient].age = this.age;
+    this.clients[this.editClient].name = this.name;
+    this.clients[this.editClient].food  = this.food;
+    this.editClient = -1;
   }
   this.age = null as any;
   this.name = "";
   this.food = "";
+  }
+
+  remove(i:number){
+   this.clients.splice(i,1);
+  }
+  edit(i:number){
+  this.age = this.clients[i].age;
+  this.name =this.clients[i].name;
+  this.food = this.clients[i].food;
+  this.editClient =i;
   }
 
 }
